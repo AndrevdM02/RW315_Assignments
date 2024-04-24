26d7cd74663d91068893371cb478d32f20ace14b
Andre <Andrevandermerwe0209@gmail.com>
2024-04-24 17:12:17 +0200

    Finalized K-means

--- Assignment3/Unsupervised.py
+++ Assignment3/Unsupervised.py
@@ -58,6 +58,105 @@ class Kmeans:
         self.labels = labels
         return self
 
+    def fit_means(self, X, means):
+
+        if self.random_state != None:
+            np.random.seed(self.random_state)
+
+        # Generate a random startpoint, for d cluster center points.
+        Mean = means
+            
+
+        # Class labels
+        labels = np.zeros(X.shape[0], dtype=int)
+
+        # An array that will contain all of the distances to d cluster points
+        dist = np.zeros([X.shape[0], self.n_clusters], dtype=float)
+
+        # Perform max_iter of K-means
+        for i in range(0, self.max_iter):
+            # Calculate Distances to d cluster points
+            for j in range(0, self.n_clusters):
+                sum = (X[:,0] - Mean[j,0])**2
+                for k in range(1, X.shape[1]):
+                    sum += (X[:,k] - Mean[j,k])**2
+                dist[:,j] = np.sqrt(sum)
+        
+            # Assign a label to each observation depending to which cluster center it is the closest to
+            # TODO
+            for j in range(0,X.shape[0]):
+                label = 0
+                for k in range(1, self.n_clusters):
+                    if dist[j,k-1]>=dist[j,k]:
+                        label = k
+                labels[j] = label
+
+            old_mean = np.copy(Mean)
+
+            # Calculate new cluster points; mean of all points beloning to the same cluster
+            for j in range(0, self.n_clusters):
+                for k in range(0, X.shape[1]):
+                    Mean[j,k] = np.mean(X[labels==j, k])
+
+            # Check for convergens
+            if (old_mean == Mean).all():
+                break
+
+        self.cluster_centers = Mean
+        self.labels = labels
+        return self
+    
+    def fit_1D(self, X, means):
+
+        if self.random_state != None:
+            np.random.seed(self.random_state)
+
+        # Generate a random startpoint, for d cluster center points.
+        Mean = means
+            
+
+        # Class labels
+        labels = np.zeros(X.shape[0], dtype=int)
+
+        # An array that will contain all of the distances to d cluster points
+        dist = np.zeros([X.shape[0], self.n_clusters], dtype=float)
+
+        # Perform max_iter of K-means
+        for i in range(0, self.max_iter):
+            # Calculate Distances to d cluster points
+            for j in range(0, self.n_clusters):
+                sum = (X[:] - Mean[j])**2
+                dist[:,j] = np.sqrt(sum)
+        
+            # Assign a label to each observation depending to which cluster center it is the closest to
+            # TODO
+            for j in range(0,X.shape[0]):
+                label = 0
+                for k in range(1, self.n_clusters):
+                    if dist[j,k-1]>=dist[j,k]:
+                        label = k
+                labels[j] = label
+
+            old_mean = np.copy(Mean)
+
+            # Calculate new cluster points; mean of all points beloning to the same cluster
+            for j in range(0, self.n_clusters):
+                Mean[j] = np.mean(X[labels==j])
+
+            # Check for convergens
+            if (old_mean == Mean).all():
+                # At what iteration convergence was achieved
+                # print(i+1)
+                break
+
+            # Cluster mean at i_th iteration
+            if i == 6:
+                print(Mean)
+
+        self.cluster_centers = Mean
+        self.labels = labels
+        return self
+
     def decision_boundary(self, means=None):
         Mean = means
 
